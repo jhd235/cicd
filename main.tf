dd0a552d5605d3f3ca32a029c59a20e886f9bea2
jhd235 <105744886+jhd235@users.noreply.github.com>
2025-02-06 18:43:55 +0500

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    github = {
+      source  = "integrations/github"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "github" {
+  token = var.github_token
+}
+
+resource "github_repository_pages" "main" {
+  repository = var.repository_name
+  source {
+    branch = "gh-pages"
+    path   = "/"
+  }
+}
